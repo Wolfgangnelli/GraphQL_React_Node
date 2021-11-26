37c46d66fabb34c6387c04146e06f3908d6ce14d
AndreaS <andrea.solfanelli90@gmail.com>
2021-11-26 23:45:23 +0100

    nested queries

--- schema/schema.js
+++ schema/schema.js
@@ -8,12 +8,29 @@ const users = [
   { id: "47", firstName: "Samantha", age: 21 },
 ];
 
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+  },
+});
+
 const UserType = new GraphQLObjectType({
   name: "User",
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parent, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parent.companyId}`)
+          .then((res) => res.data);
+      },
+    },
   },
 });
 
